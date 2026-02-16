281806d515a9437984621e3925a91f308516052c
LimJaeHyun <guswogus81@gmail.com>
2026-02-16 16:57:56 +0900

    스프링 입문 : 회원 관리 예제

---
+++ [01] - Spring Intoduction/hello-spring/src/main/java/hello/hello_spring/controller/HomeController.java
@@ -0,0 +1,12 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public  String home(){
+        return "home"; // templates에 있는 home.html이 호출된다
+    }
+}
